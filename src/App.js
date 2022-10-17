fe16a9cafd59a537a9018bab1211f0d2aa2047f8
greenwhale00 <goreakay@gmail.com>
2022-10-17 23:13:28 +0530

    221017

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
 
-import React from 'react'
+import Wrapper from './pages/Wrapper';
 
 const App = () => {
   return (
-    <div>App</div>
+    <Wrapper>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/sub01' element={<Magslide />} />
+        <Route path='/sub02' element={<Store />} />
+
+      </Routes>
+
+      <Footer />
+    </Wrapper>
   )
 }
 
