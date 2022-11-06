901eacb7d4ce973c546c24f775c4d3eb12bcdfb9
greenwhale00 <goreakay@gmail.com>
2022-11-06 20:45:21 +0530

    221107

--- src/App.js
+++ src/App.js
@@ -1,24 +1,106 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import ReactFullpage from '@fullpage/react-fullpage';
+
+import data from './portfolioDATA';
+import './common.css';
+import './Detail.scss';
 
 
-import Wrapper from './pages/Wrapper';
-import Cover from './pages/Cover';
-import Main from './pages/Main';
-import Footer from './pages/Footer';
 
 const App = () => {
+  const [num, setNum] = useState(1);
+  const [con, setCon] = useState('cover');
+  const [on, setOn] = useState(false);
+  const ac = data.map(it => it.anchor);
+
+  const Cover = () => {
+    return (
+      <div className={`cover ${on ? 'on' : ''}`}>
+        <ul>
+          {
+            data.map((it, idx) => {
+              return (
+                <li>
+                  <a href={`#${it.anchor}`} onClick={
+                    () => setOn(!on)
+                  }>{it.title}</a>
+                </li>
+              )
+            })
+          }
+        </ul>
+      </div>
+    )
+  }
   return (
-    <Wrapper>
+    <div className='FP'>
       <Cover />
-      <Routes>
-        <Route path='/' element={<Main />} />
+      <header className='header'>
+        2023 KHL portfolio {num} {con}
+      </header>
+      <button onClick={() => setOn(!on)} className={`cover_open ${on ? 'on' : ''}`}>
+        <span>커버 나오는 버튼</span>
+      </button>
+      <nav className='gnb'>
+        <ul>
+          {
+            ['cover', ...ac, 'copyright'].map((it, idx) => {
+              return (
+                <li className={num === (idx + 1) ? 'on' : ''}>
+                  <a href={`#${it}`}>{it}</a>
+                </li>
+              )
+            })
+          }
+        </ul>
+      </nav>
+      <ReactFullpage
+        //fullpage options
+        licenseKey={'YOUR_KEY_HERE'}
+        scrollingSpeed={1000} /* Options here */
+        anchors={['cover', ...ac, 'copyright']}
+        afterLoad={(origin, destination) => {
+          setNum(destination.index + 1);
+          setCon(destination.anchor)
+        }
+        }
 
+        render={({ state, fullpageApi }) => {
+          return (
+            <ReactFullpage.Wrapper>
+              <div className="section">
+                <div className='inner'>
+                  <p>Section 1 (welcome to fullpage.js)</p>
+                  <a href="#pf01">01</a>
+                  <button onClick={() => fullpageApi.moveSectionDown()}>
+                    Click me to move down
+                  </button>
+                </div>
+              </div>
+              {
+                data.map(it => {
+                  return (
+                    <div className="section">
+                      <div className='inner'>
+                        <p>{it.id}</p>
+                        {
+                          it.color?.map(color => <li style={{ background: color }}>{color}</li>)
+                        }
+                      </div>
+                    </div>
+                  )
+                })
+              }
+              <div className="section">
+                <p className='inner'>Footer</p>
+              </div>
+            </ReactFullpage.Wrapper>
+          );
+        }}
+      />
+    </div>
 
-      </Routes>
-
-      <Footer />
-    </Wrapper>
   )
 }
 
---
+++ src/portfolioDATA.js
@@ -0,0 +1,9 @@
+const PD = [
+    { id: 1, title: "pf01", content: "first homepage", link: "greenwhale00.github.io/zegna", anchor: "pf01" },
+    { id: 2, title: "pf02", content: "second homepage", link: "greenwhale00.github.io/kinfolk", anchor: "pf02" },
+    { id: 3, title: "pf03", content: "third homepage", link: "greenwhale00.github.io/balmuda", anchor: "pf03" },
+    { id: 4, title: "pf03", content: "fourth homepage", link: "greenwhale00.github.io/balmuda", anchor: "pf04" },
+]
+
+
+export default PD
